c61b76aa2f44797721e694fc060314bb39c21328
Thomas Feldmann <mail@tfeldmann.de>
2021-04-15 18:06:21 +0200

    add some more types

--- easymatch.py
+++ easymatch.py
@@ -2,60 +2,69 @@
 """
 easymatch
 """
-import json
 import re
-from argparse import ArgumentParser
 from collections import namedtuple
-
 
 # taken from the standard re module, minus "*{}"
 _special_chars = {i: "\\" + chr(i) for i in b"()[]?+-|^$\\.&~# \t\n\r\v\f"}
 
 # dict of known types
 Type = namedtuple("Type", "regex converter")
-types = {
-    "int": Type(
-        regex=r"[+-]?0|[1-9][0-9]*",
-        converter=int,
-    ),
-    "float": Type(
-        regex=r"[+-]?([0-9]*[.])?[0-9]+",
-        converter=float,
-    ),
-    "email": Type(
-        regex=r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+",
-        converter=str,
-    ),
-    "ssn": Type(
-        regex=r"(?!0{3})(?!6{3})[0-8]\d{2}-(?!0{2})\d{2}-(?!0{4})\d{4}",
-        converter=str,
-    ),
-    "ipv4": Type(
-        regex=r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}",
-        converter=str,
-    ),
-    "ipv6": Type(
-        regex=r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))",
-        converter=str,
-    ),
-}
+types = {}
 
 
 def register_type(name, regex, converter=str):
-    types[name] = Type(regex=regex, converter=converter)
+    # ensure all groups to be non-capturing
+    clean_regex = re.sub(r"(?<!\\)\((?!\?)", r"(?:", regex)
+    types[name] = Type(regex=clean_regex, converter=converter)
 
 
-def grouplist(match):
-    """ extract unnamed match groups """
-    # https://stackoverflow.com/a/53385788/300783
-    named = match.groupdict()
-    ignored_groups = set()
-    for name, index in match.re.groupindex.items():
-        if name in named:  # check twice, if it is really the named attribute.
-            ignored_groups.add(index)
-    return [
-        group for i, group in enumerate(match.groups()) if i + 1 not in ignored_groups
-    ]
+# include some types. Courtesy of https://ihateregex.io/
+register_type("int", r"[+-]?[0-9]+", int)
+register_type("float", r"[+-]?([0-9]*[.])?[0-9]+", float)
+register_type("email", r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+")
+register_type("ssn", r"(?!0{3})(?!6{3})[0-8]\d{2}-(?!0{2})\d{2}-(?!0{4})\d{4}")
+register_type(
+    "ipv4",
+    (
+        r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]"
+        r"?)){3}"
+    ),
+)
+register_type(
+    "ipv6",
+    (
+        r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA"
+        r"-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){"
+        r"1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3"
+        r"}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0"
+        r"-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:"
+        r"(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5"
+        r"]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0"
+        r"-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,"
+        r"3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
+    ),
+)
+# Visa, MasterCard, American Express, Diners Club, Discover, JCB
+register_type(
+    "ccard",
+    (
+        r"(^4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6]["
+        r"0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])"
+        r"[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)"
+    ),
+)
+register_type(
+    "bitcoin",
+    r"(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}",
+)
+register_type(
+    "url",
+    (
+        r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA"
+        r"-Z0-9()!@:%_\+.~#?&\/\/=]*)"
+    ),
+)
 
 
 class Matcher:
@@ -64,7 +73,7 @@ class Matcher:
         self.pattern = pattern
 
         # cache the compiled regex
-        self.regex = self.create_regex(pattern)
+        self.regex = self._create_regex(pattern)
         self._regex = re.compile(self.regex)
 
     def test(self, string):
@@ -76,7 +85,7 @@ class Matcher:
         if match:
             # assemble result dict
             result = match.groupdict()
-            for i, x in enumerate(grouplist(match)):
+            for i, x in enumerate(self._grouplist(match)):
                 result[i] = x
 
             # run converters
@@ -92,9 +101,7 @@ class Matcher:
             name, type_ = match.groups()
             # register this field to convert it later
             self._converters[name] = types[type_].converter
-            # ensure all groups to be non-capturing
-            regex = re.sub(r"(?<!\\)\((?!\?)", r"(?:", types[type_].regex)
-            return "(?P<%s>%s)" % (name, regex)
+            return "(?P<%s>%s)" % (name, types[type_].regex)
 
         # field without type annotation
         match = re.search(r"\{(\w+)\}", matchobj.group(0))
@@ -102,13 +109,28 @@ class Matcher:
             name = match.group(1)
             return r"(?P<%s>.*)" % name
 
-    def create_regex(self, pattern):
+    def _create_regex(self, pattern):
         self._converters.clear()  # empty converters
         result = pattern.translate(_special_chars)  # escape special chars
         result = result.replace("*", r".*")  # handle wildcards
         result = re.sub(r"\{\.\*\}", r"(.*)", result)  # handle wildcard match
         result = re.sub(r"\{([^\}]*)\}", self._field_repl, result)  # handle named match
         return "^%s$" % result
+
+    @staticmethod
+    def _grouplist(match):
+        """ extract unnamed match groups """
+        # https://stackoverflow.com/a/53385788/300783
+        named = match.groupdict()
+        ignored_groups = set()
+        for name, index in match.re.groupindex.items():
+            if name in named:  # check twice, if it is really the named attribute.
+                ignored_groups.add(index)
+        return [
+            group
+            for i, group in enumerate(match.groups())
+            if i + 1 not in ignored_groups
+        ]
 
     def __repr__(self):
         return '<Matcher("%s")>' % self.pattern
@@ -123,6 +145,9 @@ def match(pattern, string):
 
 
 if __name__ == "__main__":
+    import json
+    from argparse import ArgumentParser
+
     parser = ArgumentParser()
     parser.add_argument("pattern", help="A matching pattern")
     parser.add_argument("string", help="The string to match")
--- test_easymatch.py
+++ test_easymatch.py
@@ -67,3 +67,78 @@ def test_simple_matching():
         1: "p=1",
         "filename": "hello.js",
     }
+
+
+def test_type_int():
+    m = em.Matcher("{num:int}")
+    assert m.match("123") == {"num": 123}
+    assert m.match("-123") == {"num": -123}
+    assert not m.match("-123.0")
+    assert m.match("+123") == {"num": 123}
+    assert m.match("+000123") == {"num": 123}
+    assert m.match("0123") == {"num": 123}
+
+
+def test_type_float():
+    m = em.Matcher("{num:float}")
+    assert m.match("123.4") == {"num": 123.4}
+    assert m.match("-123.4") == {"num": -123.4}
+    assert m.match("-123.0")
+    assert m.match("+123.4") == {"num": 123.4}
+    assert m.match("+000123.4") == {"num": 123.4}
+
+
+def test_type_ipv4():
+    m = em.Matcher("{ip:ipv4}")
+    assert m.match("127.0.0.1") == {"ip": "127.0.0.1"}
+    assert m.match("192.168.1.1") == {"ip": "192.168.1.1"}
+    assert m.match("127.0.0.1") == {"ip": "127.0.0.1"}
+    assert m.match("0.0.0.0") == {"ip": "0.0.0.0"}
+    assert m.match("255.255.255.255") == {"ip": "255.255.255.255"}
+    assert not m.match("256.256.256.256")
+    assert not m.match("999.999.999.999")
+    assert not m.match("1.2.3")
+    assert m.match("1.2.3.4") == {"ip": "1.2.3.4"}
+
+
+def test_type_ccard():
+    m = em.Matcher("{card:ccard}")
+    assert m.match("4569403961014710") == {"card": "4569403961014710"}
+    assert m.match("5191914942157165") == {"card": "5191914942157165"}
+    assert m.match("370341378581367") == {"card": "370341378581367"}
+    assert m.match("38520000023237") == {"card": "38520000023237"}
+    assert m.match("6011000000000000") == {"card": "6011000000000000"}
+    assert m.match("3566002020360505") == {"card": "3566002020360505"}
+    assert not m.match("1234566660000222")
+
+
+def test_type_bitcoin():
+    m = em.Matcher("{coin:bitcoin}")
+    assert m.match("1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY") == {
+        "coin": "1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY"
+    }
+    assert not m.match("loremipsum")
+    assert m.match("16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe") == {
+        "coin": "16ftSEQ4ctQFDtVZiUBusQUjRrGhM3JYwe"
+    }
+    assert m.match("1EBHA1ckUWzNKN7BMfDwGTx6GKEbADUozX") == {
+        "coin": "1EBHA1ckUWzNKN7BMfDwGTx6GKEbADUozX"
+    }
+    assert not m.match("0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae")
+    assert m.match("bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq") == {
+        "coin": "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq"
+    }
+
+
+def test_type_url():
+    m = em.Matcher("{url:url}")
+    assert not m.match("abcdef")
+    assert not m.match("www.whatever.com")
+    assert m.match("https://github.com/geongeorge/i-hate-regex") == {
+        "url": "https://github.com/geongeorge/i-hate-regex"
+    }
+    assert m.match("https://www.facebook.com/") == {"url": "https://www.facebook.com/"}
+    assert m.match("https://www.google.com/") == {"url": "https://www.google.com/"}
+    assert m.match("https://xkcd.com/2293/") == {"url": "https://xkcd.com/2293/"}
+    assert not m.match("https://this-shouldn't.match@example.com")
+    assert m.match("http://www.example.com/") == {"url": "http://www.example.com/"}
